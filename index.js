e85c9c27661547a001a68ec52a8794200a16a873
khaledrezk <khaledkra2112@gmail.com>
2021-01-21 18:51:12 +0200

    Implemented the validateSubmission function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Joi = require("joi"); // for validation
 
-function validate_submission(body) {
+function validateSubmission(body) {
     const schema = Joi.object({
         lang: Joi.string().equal("C++").required(),// programming lanugage initially we support only C++
         timeLimit: Joi.number().min(.5).max(10).required(),// the timelimit to run code in seconds
