5040c591c09f736852f58f96d6e2103dcaacd376
khaledrezk <khaledkra2112@gmail.com>
2021-01-21 18:53:43 +0200

    Implemented the runCPPCode function to excecute c++ code

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const Joi = require("joi"); // for validation
+const fs = require("fs");
+const util = require("util");
+const exec = util.promisify(require('child_process').exec);
+const writeFile = util.promisify(fs.writeFile);
 
 function validateSubmission(body) {
     const schema = Joi.object({
@@ -9,5 +13,75 @@ function validateSubmission(body) {
         input: Joi.string().max(1000000).required()// the input to the program
     });
     return schema.validate(body);
+}
+
+
+
+
+let countSubmissions = 0;
+const submissionsDirectory = "submissions/";
+
+if(!fs.existsSync(submissionsDirectory))// create a directory to store submissions
+    fs.mkdirSync(submissionsDirectory);
+
+
+async function runCPPCode(sourceCode, input, timeLimit, memoryLimit = 512){//memory limit is not handled yet
+    const mySubmission = countSubmissions++;// current submission number not same as code ID
+    const myDirectory = `${submissionsDirectory}${mySubmission}/`;// the directory of storing submission data
+    const codePath = `${myDirectory + mySubmission}.cpp`;
+    const programPath = `${myDirectory + mySubmission}`; 
+    const inputFilePath = `${myDirectory + mySubmission}.in`;
+
+    if(!fs.existsSync(myDirectory))// create the directory to run my code
+        fs.mkdirSync(myDirectory);
+
+    try{
+        await writeFile(codePath, sourceCode);// write the code to codePath
+        await writeFile(inputFilePath, input);// write input to input path
+    }
+    catch(err){
+        if(err){
+            console.error(err);// log the error on the system
+            throw err;
+        }
+    }
+
+    try{
+        // Compile the C++ Code with a maximum of 3 secnods in compilation
+        await exec(`timeout 3 g++ ${codePath} -o ${programPath}`);// compile
+    }
+    catch(err) {
+        if(err){
+            // status is 200 as no errors occured
+            return {codeStatus : "Compilation Error!"};
+        }
+    }
+
+    const time_before = Date.now();
+    // give extra .5 second to make sure that the exit was made by TLE not RTE
+    try{
+        console.log("Time limit :", timeLimit);
+        const {stdout} = await exec(`timeout ${parseFloat(timeLimit) + .5} ./${programPath} < ${inputFilePath}`);
+        const time_after = Date.now();
+        // used time computation is not very accurate but ok for now, should be updated in the future.
+        const used_time = (time_after - time_before);
+        // used time is in ms while timeLimit is in seconds
+        if(used_time > timeLimit * 1000){
+            console.log("used time :", used_time);
+            return {codeStatus: "Time Limit Exceeded!"};
+        }
+        return {codeStatus : "Accepted", output : stdout, usedTime : used_time};
+        // we should decide whether we will remove the sumbission code after or not
+    }
+    catch(err){
+        const time_after = Date.now();
+        const used_time = (time_after - time_before);
+        if(used_time > timeLimit * 1000){
+            return {codeStatus: "Time Limit Exceeded!"};
+        }
+        else {
+            return {codeStatus: "Run Time Error!"};
+        }
+    }
 }
 
