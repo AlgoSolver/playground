7c62d6fe85d0ea55555631b38a1d180971765d82
s403o <eslam.adel@protonmail.com>
2021-01-31 18:34:46 +0200

    add compose & update .ignore & run script

---
+++ run_docker.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#run app at localhost:1234
+
+
+docker run -it --rm \
+-v ${PWD}:/app \
+-v /app/node_modules \
+-p 1234:3000 \
+-e CHOKIDAR_USEPOLLING=true \
+s403o/algosolver-playground:dev
