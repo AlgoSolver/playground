b741ca97bbb60a68da5e94e62157a426599b016f
khaledrezk <khaledkra2112@gmail.com>
2021-01-31 04:31:16 +0200

    Implemented validate Checker

--- index.js
+++ index.js
@@ -18,6 +18,19 @@ function validateSubmission(body) {
     return schema.validate(body);
 }
 
+function validateChecker(body) {
+    const schema = Joi.object({
+        lang: Joi.string().equal("C++").required(),// programming lanugage initially we support only C++
+        timeLimit: Joi.number().min(.5).max(10).required(),// the timelimit to run code in seconds
+        memoryLimit: Joi.number().min(1024).max(1024*1024), // number of KBs maximum is 1G isn't supported initially
+        sourceCode: Joi.string().max(100000).required(), // the source code to run
+        input: Joi.string().max(1000000).required(),// the input to the program
+        // all obove is the same as problems.
+        userOutput: Joi.string().max(1000000).required(),// the answer from user
+        juryOutput: Joi.string().max(1000000).required(),// the expected answer from jury
+    });
+}
+
 let countSubmissions = 0;
 const submissionsDirectory = "submissions/";
 
